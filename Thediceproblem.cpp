e94dfcac79d69feffb0c1c745a8cb526d7b4fafd
Shabina Siddiqui <siddiquishabina10@gmail.com>
2022-01-04 17:50:06 +0530

    The dice problem

---
+++ Thediceproblem.cpp
@@ -0,0 +1,11 @@
+//problem link:  https://practice.geeksforgeeks.org/problems/the-dice-problem2316/1/?page=1&query=page1#
+class Solution
+{
+public:
+    int oppositeFaceOfDice(int N)
+    {
+       
+        int ans = 7-N;
+        return ans;
+    }
+};
